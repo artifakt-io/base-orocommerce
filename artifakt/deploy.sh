4d0ba33a9a49554537110c6f9160a8293f0be5f6
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-05-20 16:11:15 +0200

    Edit auto deploy part in deploy.sh

--- artifakt/deploy.sh
+++ artifakt/deploy.sh
@@ -2,6 +2,7 @@
 
 if [[ $AUTO_SETUP -eq 1 ]]; then
  if [[ ! -z $AUTO_SETUP_DOMAIN ]]; then
+    sed -i "s/installed: true/installed: false/g" config/parameters.yml
     echo "Removing cache folder"
     sudo rm -rf var/cache/*
     echo "Starting installation with login: admin@example.com, password artifakt123 and domain $AUTO_SETUP_DOMAIN"
@@ -16,6 +17,8 @@ if [[ $AUTO_SETUP -eq 1 ]]; then
     --user-lastname="Doe" \
     --user-password="artifakt123" \
     --application-url="$AUTO_SETUP_DOMAIN"
+
+    sudo service supervisord restart
 else
     echo "To start auto setup please add an environment variable AUTO_SETUP_DOMAIN with the full domain url (with a slash at the end)"
  fi
