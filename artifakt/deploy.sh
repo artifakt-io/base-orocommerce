e0ceadcd28eb03c6a361b917f26a559cd7728301
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-05-20 15:56:06 +0200

    Edit auto deploy part in deploy.sh

--- artifakt/deploy.sh
+++ artifakt/deploy.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 if [[ $AUTO_SETUP -eq 1 ]]; then
- if [[ -z $AUTO_SETUP_DOMAIN ]]; then
-        php bin/console oro:install \
-        --env=prod \
-        --timeout=600 \
-        --language=en \
-        --formatting-code="en_US" \
-        --user-name="admin" \
-        --user-email="admin@example.com" \
-        --user-firstname="John" \
-        --user-lastname="Doe" \
-        --user-password="PleaseReplaceWithSomeStrongPassword" \
-        --application-url="$AUTO_SETUP_DOMAIN"
-    else
+ if [[ ! -z $AUTO_SETUP_DOMAIN ]]; then
+    echo "Removing cache folder"
+    sudo rm -rf var/cache/*
+    echo "Starting installation with login: admin@example.com, password artifakt123 and domain $AUTO_SETUP_DOMAIN"
+    php bin/console oro:install \
+    --env=prod \
+    --timeout=600 \
+    --language=en \
+    --formatting-code="en_US" \
+    --user-name="admin" \
+    --user-email="admin@example.com" \
+    --user-firstname="John" \
+    --user-lastname="Doe" \
+    --user-password="artifakt123" \
+    --application-url="$AUTO_SETUP_DOMAIN"
+else
     echo "To start auto setup please add an environment variable AUTO_SETUP_DOMAIN with the full domain url (with a slash at the end)"
  fi
 fi
