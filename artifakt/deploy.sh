d62056fd726284af1ec001a96ec9119fa3b1fa92
Aymeric Mathéossian <aymeric.matheossian@artifakt.io>
2021-05-20 15:39:34 +0200

    Edit deploy script

--- artifakt/deploy.sh
+++ artifakt/deploy.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
 
+if [[ $AUTO_SETUP -eq 1 ]]; then
+ if [[ -z $AUTO_SETUP_DOMAIN ]]; then
+
+    # Delete all tables
+    
+        php bin/console oro:install \
+        --env=prod \
+        --timeout=600 \
+        --language=en \
+        --formatting-code="en_US" \
+        --user-name="admin" \
+        --user-email="admin@example.com" \
+        --user-firstname="John" \
+        --user-lastname="Doe" \
+        --user-password="PleaseReplaceWithSomeStrongPassword" \
+        --application-url="$AUTO_SETUP_DOMAIN"
+    else
+    echo "To start auto setup please add an environment variable AUTO_SETUP_DOMAIN with the full domain url (with a slash at the end)"
+ fi
+fi
